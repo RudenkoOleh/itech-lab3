df9a398f62f7eee46fe2ddd81ba89fd26d5df3ae
Oleh <oleh.rudenko1@nure.ua>
2023-11-07 23:27:38 +0200

    added functionality in separate file script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculateAverageExperience(event) {
+    event.preventDefault();
+    let hireDates = document.querySelectorAll('input[name="hireDate"]');
+    let currentDate = new Date();
+    let totalExperience = 0;
+
+    for (let i = 0; i < hireDates.length; i++) {
+        let hireDate = new Date(hireDates[i].value);
+        let yearsOfWork = currentDate.getFullYear() - hireDate.getFullYear();
+        totalExperience += yearsOfWork;
+    }
+
+    let averageExperience = Math.round(totalExperience / hireDates.length);
+    document.getElementById("result").textContent = "Середній стаж роботи працівників: " + averageExperience + " років";
+}
